9a44c6baf656c6736a5525100779ff9e7c731073
Ana Ibarra <ana.ibarra@outlook.fr>
2020-10-29 11:08:04 +0100

    'text'

--- app.py
+++ app.py
@@ -3,12 +3,33 @@ import os
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+from dash.dependencies import Input, Output, State
 
-# external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
-# app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
-# server = app.server
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+server = app.server
+
+
+app.layout = html.Div([
+    dcc.Input(id='input-1-state', type='text', value='Montréal'),
+    dcc.Input(id='input-2-state', type='text', value='Canada'),
+    html.Button(id='submit-button-state', n_clicks=0, children='Submit'),
+    html.Div(id='output-state')
+])
+
+# @app.callback(Output('output-state', 'children'),
+#               [Input('submit-button-state', 'n_clicks')],
+#               [State('input-1-state', 'value'),
+#                State('input-2-state', 'value')])
+# def update_output(n_clicks, input1, input2):
+#     return u'''
+#         The Button has been pressed {} times,
+#         Input 1 is "{}",
+#         and Input 2 is "{}"
+#     '''.format(n_clicks, input1, input2)
 
 # app.layout = html.Div([
 #     html.H2('Hello World\n I did it!'),
@@ -19,37 +40,12 @@ import dash_html_components as html
 #     ),
 #     html.Div(id='display-value')
 # ])
-from dash.dependencies import Input, Output, State
-
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-
-app.layout = html.Div([
-    dcc.Input(id='input-1-state', type='text', value='Montréal'),
-    dcc.Input(id='input-2-state', type='text', value='Canada'),
-    html.Button(id='submit-button-state', n_clicks=0, children='Submit'),
-    html.Div(id='output-state')
-])
-
-@app.callback(Output('output-state', 'children'),
-              [Input('submit-button-state', 'n_clicks')],
-              [State('input-1-state', 'value'),
-               State('input-2-state', 'value')])
-def update_output(n_clicks, input1, input2):
-    return u'''
-        The Button has been pressed {} times,
-        Input 1 is "{}",
-        and Input 2 is "{}"
-    '''.format(n_clicks, input1, input2)
-
-# if __name__ == '__main__':
-#     app.run_server(debug=True)
-
 # @app.callback(dash.dependencies.Output('display-value', 'children'),
 #               [dash.dependencies.Input('dropdown', 'value')])
 # def display_value(value):
 #     return 'You have selected "{}"'.format(value)
+
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
