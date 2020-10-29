e9c5e87e7bcfb398cb98ad844061fe75b7f2f070
Ana Ibarra <ana.ibarra@outlook.fr>
2020-10-29 13:47:36 +0100

    'actualiza'

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ app.layout = html.Div(children=[
     html.H1(children='Questions Classification'),
 
     html.Div(children='''
-        P5: A web application framework, Open Classrrom .
+        P5: A web application framework, from Openclassroom .
     '''),
 
     dcc.Input(id='input-1-state',
@@ -42,26 +42,7 @@ app.layout = html.Div(children=[
 #               [Input('submit-button-state', 'n_clicks')],
 #               [State('input-1-state', 'value'),
 #                State('input-2-state', 'value')])
-# def update_output(n_clicks, input1, input2):
-#     return u'''
-#         The Button has been pressed {} times,
-#         Input 1 is "{}",
-#         and Input 2 is "{}"
-#     '''.format(n_clicks, input1, input2)
 
-# app.layout = html.Div([
-#     html.H2('Hello World\n I did it!'),
-#     dcc.Dropdown(
-#         id='dropdown',
-#         options=[{'label': i, 'value': i} for i in ['LA', 'NYC', 'MTL']],
-#         value='LA'
-#     ),
-#     html.Div(id='display-value')
-# ])
-# @app.callback(dash.dependencies.Output('display-value', 'children'),
-#               [dash.dependencies.Input('dropdown', 'value')])
-# def display_value(value):
-#     return 'You have selected "{}"'.format(value)
 
 
 
