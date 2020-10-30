c8a1e0a366566d63796e1a7e2fb5112b473ffa4e
Ana Ibarra <ana.ibarra@outlook.fr>
2020-10-30 12:08:26 +0100

    'comentario'

--- app.py
+++ app.py
@@ -5,26 +5,21 @@ import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output, State
 
-
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 server = app.server
 
-
 app.layout = html.Div(children=[
-    html.H1(children='Questions Classification'),
-
+    html.H1(children='Questions Classification Stackoverflow'),
     html.Div(children='''
         P5: A web application framework, from Openclassroom .
     '''),
-
     dcc.Input(id='input-1-state',
               type='text', 
               value='Titre', 
-              style={'width': '80%', 'marginBottom': 50, 'marginTop': 25}),
-    
+              style={'width': '80%', 'marginBottom': 10, 'marginTop': 10}),    
     dcc.Textarea(
         id='input-2-state', 
         value='Text',
@@ -32,8 +27,7 @@ app.layout = html.Div(children=[
 #     dcc.Textarea(
 #         id='input-1-state', 
 #         value='Titre',
-#         style={'width': '100%', 'height': 50}),
-    
+#         style={'width': '100%', 'height': 50}),    
     html.Button(id='submit-button-state', n_clicks=0, children='Submit'),
     html.Div(id='textarea-example-output', style={'whiteSpace': 'pre-line'})
 ])
