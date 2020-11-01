3915198b3323575914ff55fe0d8e0e3e3ba1bed9
Ana Ibarra <ana.ibarra@outlook.fr>
2020-11-01 21:36:59 +0100

    'funciones13'

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ import os
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-from functions2 import body_clean
-from functions2 import tags_prediction
+# from functions2 import body_clean
+# from functions2 import tags_prediction
 
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
