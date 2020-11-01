5e012d187de81f6b9f5dcf1135754db153063d1b
Ana Ibarra <ana.ibarra@outlook.fr>
2020-11-01 23:40:02 +0100

    'funciones20'

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import os
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+from dash.dependencies import Input, Output, State
+
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
