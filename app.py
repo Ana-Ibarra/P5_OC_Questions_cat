4094e8a630ebc4f33829ced6e48ad3322f2cec39
Ana Ibarra <ana.ibarra@outlook.fr>
2020-11-01 16:18:48 +0100

    'funciones'

--- app.py
+++ app.py
@@ -16,29 +16,28 @@ app.layout = html.Div(children=[
     html.Div(children='''
         P5: A web application framework, from Openclassroom .
     '''),
-    dcc.Input(id='input-1-state',
-              type='text', 
-              value='Titre', 
-              style={'width': '80%', 'marginBottom': 10, 'marginTop': 10}),    
+#     dcc.Input(id='input-1-state',
+#               type='text', 
+#               value='Titre', 
+#               style={'width': '80%', 'marginBottom': 10, 'marginTop': 10}), 
     dcc.Textarea(
-        id='input-2-state', 
-        value='Text',
-        style={'width': '100%', 'height': 300}),
-#     dcc.Textarea(
-#         id='input-1-state', 
-#         value='Titre',
-#         style={'width': '100%', 'height': 50}),    
+        id='input-1-state', value='Title', style={'width': '100%', 'height': 50}), 
+    dcc.Textarea(
+        id='input-2-state', value='Text', style={'width': '100%', 'height': 300}),
+   
     html.Button(id='submit-button-state', n_clicks=0, children='Submit'),
-    html.Div(id='textarea-example-output', style={'whiteSpace': 'pre-line'})
+    html.Div(id='output-state', style={'whiteSpace': 'pre-line'})
 ])
 
-# @app.callback(Output('output-state', 'children'),
-#               [Input('submit-button-state', 'n_clicks')],
-#               [State('input-1-state', 'value'),
-#                State('input-2-state', 'value')])
+@app.callback(Output('output-state', 'children'),
+              [Input('submit-button-state', 'n_clicks')],
+              [State('input-1-state', 'value'),
+               State('input-2-state', 'value')])
 
-
-
+def update_output('input-1-state','input-2-state'):
+    body = body_clean('input-1-state', 'input-2-state')
+    output = tags_prediction(body)
+    return output
 
 if __name__ == '__main__':
     app.run_server(debug=True)
