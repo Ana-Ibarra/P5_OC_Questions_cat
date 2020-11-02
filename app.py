4fe88ecf3d9afe5e6ede086cbabb04f83906a892
Ana Ibarra <ana.ibarra@outlook.fr>
2020-11-02 01:04:33 +0100

    'funciones20'

--- app.py
+++ app.py
@@ -27,6 +27,64 @@ app.layout = html.Div(children=[
     html.Div(id='Tags_output', style={'whiteSpace': 'pre-line'})
     ])
 
+###**********************************************************#####
+###
+###**********************************************************#####
+
+# import pandas as pd  
+# import numpy as np
+
+# import joblib
+# import re
+# import nltk
+from nltk.corpus import stopwords
+from nltk.stem.snowball import SnowballStemmer
+from nltk.stem import WordNetLemmatizer
+from bs4 import BeautifulSoup
+
+# Create a set with stopwords from ntkl and personalized dict
+stops = set(stopwords.words("english"))   
+custom_words = ['use','would','x','want','way','like','work','get','one',
+                'new','code','need','someth','test','good','make','always',
+                'problem','take','best','anyone','given','look','also',
+                'well','give','user','value','without','know','abcde',
+                'any','does','exampl','try','ani','do','doe','e','v','j'
+                'file','will', 'hi', 'hello','question']   
+stop_words = stops.union(set(custom_words))
+
+
+def body_clean(title, text):
+    body = [title + text]
+    body = ''.join(body)
+    body = re.sub('\+\+','plusplus', body) 
+    body = re.sub('#','sharp', body)
+    letters_only = re.sub("[^a-zA-Z]", " ", body)  
+    words = letters_only.lower().split()         
+    words = [w for w in words if not w in stop_words] 
+    wnl = WordNetLemmatizer()
+    words = [wnl.lemmatize(w) for w in words]
+    stemmer = SnowballStemmer("english")
+    words = [stemmer.stem(word) for word in words]      
+    words = [w for w in words if not w in stop_words]
+    return ( " ".join(words))
+
+def tags_prediction(body):
+    X = pd.Series(body)
+    # TF-IDF Vectorization
+    tfidf = joblib.load('tfidf_model.sav')
+    X = tfidf.transform(X)
+    # Multilabelled tags
+    mlb = joblib.load('multilabelling_model.sav')
+    # Linear SVC model
+    svc_model = joblib.load('finalized_model.sav')
+    pred_svc = svc_model.predict(X)
+    # Visualization of tags
+    return mlb.inverse_transform(pred_svc)
+
+###**********************************************************#####
+###
+###**********************************************************#####
+
 
 @app.callback(Output('Tags_output', 'children'),
               [Input('submit-button-state', 'n_clicks')],
@@ -34,10 +92,8 @@ app.layout = html.Div(children=[
                State('input-2-state', 'value')])
 def update_output(n_clicks, input1, input2):
     if n_clicks > 0:
-        from functions2 import body_clean
-        from functions2 import tags_prediction
         body = body_clean(input1, input2)
-#         output = tags_prediction(body)
+        output = tags_prediction(body)
         return u'''N click= {}, Your tags are : \n{}'''.format(n_clicks,body)
 
 
