e265f2987282ad5ed50ab1b84ed039c98989fc89
Ana Ibarra <ana.ibarra@outlook.fr>
2020-11-03 15:18:17 +0100

    'try'

--- app.py
+++ app.py
@@ -10,28 +10,33 @@ from functions import *
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+colors = {'background': '#a23530',
+          'text': '#FFFFFF'}
+
 app.config.suppress_callback_exceptions = True
+
 server = app.server
 
 style = {'maxWidth': '960px', 'margin': 'auto'}
+
 app.layout = html.Div(children=[
-    html.H1(children='Questions Classification Stackoverflow'),
+    html.H1(children="Project 5: Stackoverflow's questions classification"),
     html.Div(children='''
-        P5: A web application framework, from Openclassroom .
+        A web application framework, from Openclassroom.
     '''),
     dcc.Input(
-        id='input-1-state', type='text', placeholder='Title of the question', value= 'Hi my name is Juan!',
-        style={'width': '80%', 'marginBottom': 10, 'marginTop': 10}), 
-   dcc.Textarea(
-        id='input-2-state', placeholder='Here you have to put all the explanations about your questions including the description and so on...do no hesitate into use as many space as needed',
-       value='I want to know if my mom loves me or not but I use python and C++ to ask my existential java question',
-       style={'width': '100%', 'height': 300}),
+        id='input-1-state', type='text', placeholder='Title of the question', value= 'Hi my name is Ana!',
+        style={'width': '80%', 'marginBottom': 10, 'marginTop': 10,'backgroundColor': colors['background'],'color': colors['text']}), 
+    dcc.Textarea(
+        id='input-2-state', placeholder='Here write the question',
+       value="I want to know if I'll have a new screen next month but I use python and C++ to ask my existential java question",
+       style={'width': '100%', 'height': 200,'backgroundColor': colors['background'],'color': colors['text']}),
    
     html.Button(id='submit-button-state', n_clicks=0, children='Submit'),
     html.Div(id='Tags_output', style={'whiteSpace': 'pre-line'})
     ])
 
-    
 @app.callback(Output('Tags_output', 'children'),
               [Input('submit-button-state', 'n_clicks')],
               [State('input-1-state', 'value'),
@@ -40,7 +45,7 @@ def update_output(n_clicks, input1, input2):
     if n_clicks > 0 and len(input1)>0 and len(input2)>0:
         body = body_clean(input1, input2)
         output = tags_prediction(body) 
-        return u'''Your cleaned tags are : \n{} \n Tags: {}\n'''.format(body,output)
+        return u'''Your treated tags are : Tags: {}\n'''.format(output)
     elif n_clicks == 0 :
         return u'''PLEASE TYPE SOME TEXT'''
     elif len(input1)==0 or len(input2)==0:
