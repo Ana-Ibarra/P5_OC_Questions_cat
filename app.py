81e36b9c02a62e46da14d7e55f9d3427738348db
Ana Ibarra <ana.ibarra@outlook.fr>
2020-11-03 14:03:02 +0100

    'try'

--- app.py
+++ app.py
@@ -20,11 +20,11 @@ app.layout = html.Div(children=[
         P5: A web application framework, from Openclassroom .
     '''),
     dcc.Input(
-        id='input-1-state', type='text', placeholder='This is the title of the question', #value= 'Hi my name is Juan!',
+        id='input-1-state', type='text', placeholder='Title of the question', value= 'Hi my name is Juan!',
         style={'width': '80%', 'marginBottom': 10, 'marginTop': 10}), 
    dcc.Textarea(
         id='input-2-state', placeholder='Here you have to put all the explanations about your questions including the description and so on...do no hesitate into use as many space as needed',
-       #value='I want to know if my mom loves me or not but I use python and C++ to ask my java question',
+       value='I want to know if my mom loves me or not but I use python and C++ to ask my existential java question',
        style={'width': '100%', 'height': 300}),
    
     html.Button(id='submit-button-state', n_clicks=0, children='Submit'),
@@ -40,7 +40,7 @@ def update_output(n_clicks, input1, input2):
     if n_clicks > 0 and len(input1)>0 and len(input2)>0:
         body = body_clean(input1, input2)
         output = tags_prediction(body) 
-        return u'''N click= {}, Your cleaned tags are : \n{} \n Tags: {}\n\n LO HICE!!'''.format(n_clicks,body,output)
+        return u'''Your cleaned tags are : \n{} \n Tags: {}\n'''.format(body,output)
     elif n_clicks == 0 :
         return u'''PLEASE TYPE SOME TEXT'''
     elif len(input1)==0 or len(input2)==0:
